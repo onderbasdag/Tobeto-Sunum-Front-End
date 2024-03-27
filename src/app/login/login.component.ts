bab217a8b5d6d0dc3e80d8cfa4cb80bbb64ced2e
muzeyyen <muzeyyenacarkara@gmail.com>
2024-03-27 23:47:56 +0300

    init request component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../service/login/login.service';
 import { Observer } from 'rxjs';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -9,41 +12,61 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent implements OnInit {
-  loginForm: FormGroup = new FormGroup({});
+  constructor(private fb: FormBuilder, private loginService: LoginService, private router: Router,private http: HttpClient) { }
 
-  constructor(private formBuilder: FormBuilder, private loginService: LoginService) { }
+  loginForm = this.fb.group({
+    email: 'levent',
+    password: 'levent',
+  });
 
+  
   ngOnInit(): void {
     console.log("baslatiliyor..");
     this.initializeForm();
   }
 
   initializeForm(): void {
-    this.loginForm = this.formBuilder.group({
+    this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
     });    
   
   }
 
-  onSubmit(): void {    
-    if (this.loginForm.valid) {
-      const email = this.loginForm.get('email')!.value; // Nullability kontrolü için ! işareti kullanıldı
-      const password = this.loginForm.get('password')!.value;
-      
-      // Form submit edildiğinde login metodu çağrılır
-      this.loginService.login(email, password).subscribe({
-        next: (value) => {
-          console.log('Value:', value);
-        },
-        error: (error) => {
-          console.error('Error:', error);
-        },
-        complete: () => {
-          console.log('Completed');
-        }
-      });
-    }
-  }
+  
+
+ login() {
+  
+ }
+
+
+ onSubmit(): void {
+  console.log('hello');
+  
+   const email = this.loginForm.get('email')!.toString() as string
+      const password = this.loginForm.get('password')!.toString() as string
+
+    const loginData = { email }; // Bir istek gövdesi nesnesi oluşturun
+
+    this.http.post<any>('http://localhost:8080/api/v1/employee/add', loginData, { // İstekler için HttpClient kullanın
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    }).subscribe({
+      next: (value) => {
+        console.log('Başarılı Giriş:', value);
+        // Başarılı giriş yanıtını yönetin (örneğin, başka bir sayfaya yönlendirin)
+      },
+      error: (error) => {
+        console.error('Giriş Hatası:', error);
+        // Giriş hatalarını yönetin (örneğin, kullanıcıya hata mesajı gösterin)
+      },
+      complete: () => {
+        console.log('Giriş isteği tamamlandı');
+      }
+    });
+  
+}
+
+  
+ 
 }
   
